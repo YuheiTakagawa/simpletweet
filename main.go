3d21e2e6730bd905fb03f903a19fce91623235ff
Yuhei Takagawa <YuheiTakagawa@users.noreply.github.com>
2018-10-24 18:34:09 +0900

    [add] option for Long text

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/garyburd/go-oauth/oauth"
+	flags "github.com/jessevdk/go-flags"
 )
 
 var strurl = "https://api.twitter.com/1.1/statuses/update.json"
@@ -15,6 +16,12 @@ var oauthClient = oauth.Client{
 	ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authorize",
 	TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
 }
+
+type Options struct {
+	Longs bool `short:"l" long:"long" description:"Long text"`
+}
+
+var opts Options
 
 func HttpPost(client *http.Client, cred *oauth.Credentials, str string) error {
 	from := url.Values{}
@@ -32,7 +39,19 @@ func HttpPost(client *http.Client, cred *oauth.Credentials, str string) error {
 }
 
 func main() {
-	tweet := os.Args[1]
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	var tweet string
+	if opts.Longs {
+		fmt.Println("Please Long Text.")
+		tweet = long()
+	} else {
+		tweet = short()
+	}
+
 	httpClient := http.DefaultClient
 	credentials := &oauth.Credentials{
 		Token:  access_token,
@@ -42,8 +61,16 @@ func main() {
 	oauthClient.Credentials.Token = consumer_key
 	oauthClient.Credentials.Secret = consumer_secret
 
-	err := HttpPost(httpClient, credentials, tweet)
+	err = HttpPost(httpClient, credentials, tweet)
 	if err != nil {
 		fmt.Printf("Error")
 	}
+}
+
+func short() string {
+	return os.Args[1]
+}
+
+func long() string {
+	return os.Args[1]
 }
